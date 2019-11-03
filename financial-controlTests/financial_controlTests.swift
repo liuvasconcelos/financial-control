126604ff92601d08db97d1b7879c986620c13cb3
liuvasconcelos <livia.vasconcelos@stant.com.br>
2019-11-03 15:57:31 -0300

    Initial Commit

---
+++ financial-controlTests/financial_controlTests.swift
@@ -0,0 +1,34 @@
+//
+//  financial_controlTests.swift
+//  financial-controlTests
+//
+//  Created by Livia Vasconcelos on 03/11/19.
+//  Copyright © 2019 Livia Vasconcelos. All rights reserved.
+//
+
+import XCTest
+@testable import financial_control
+
+class financial_controlTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
